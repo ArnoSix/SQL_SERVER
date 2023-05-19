5195be20a1a7aab564d81a0d5a2af2f2de1bb2da
Jesus Huerta <53490517+ArnoSix@users.noreply.github.com>
2023-05-18 20:48:41 -0400

    Delete SQLCMD.bat

--- SQLCMD.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-sqlcmd -S PC-RAYZEN -U Jesus  -P jesus1234  -d AdventureWorks2019  -s "," -W -Q "SELECT TOP 100 * FROM Vista_Prueba" -o "C:\Users\Jesus\OneDrive\Escritorio\%date% output.csv"
