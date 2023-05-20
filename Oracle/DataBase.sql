b26d5599d0eb63acc3ff6b950cd0107c964ff6bc
Jesus Huerta <53490517+ArnoSix@users.noreply.github.com>
2023-05-19 21:07:07 -0400

    Create DataBase.sql

---
+++ Oracle/DataBase.sql
@@ -0,0 +1,14 @@
+alter TABLE adventurework2019 add (
+
+hora varchar(20))
+
+     
+CREATE TABLE adventurework2019 (
+  id int,
+  producto varchar(50)
+  numero_producto varchar(25)
+  ubicacion varchar(50)
+  descripcion varchar(400)
+  fecha date
+  
+)
